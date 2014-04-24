9298ce9bb586c1a1077cd9f746bed951b433d741
HarukaSato <sundy0315@gmail.com>
2014-04-24 14:32:46 +0900

    have a good dayを付け足した

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var helloWorld = function(){
+	var message = document.getElementById("haveagoodday");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorldButton = document.getElementById("showHaveagoodday");
+showHelloWorldButton.addEventListener("click", helloWorld);
